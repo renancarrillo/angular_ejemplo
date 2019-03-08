64e8c38e73fee6cc5f4135a0f1b0d1e9bcbc8129
Renan Carrillo <rcarrillo@quantumbit.mx>
2019-03-08 11:53:34 -0700

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EjemploService } from './ejemplo.service';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +9,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	calificaciones: any;
 	mostrar_calificaciones: boolean;
+	animales: any;
   title = 'Formulario';
 
   Nombre = "Renan";
 
-  constructor(){
+  constructor(private servicio_animales: EjemploService){
   		this.mostrar_calificaciones = true;
   		this.calificaciones = [10,9,8,8,7,6,8,9,"renan"];
+  }
+
+  ngOnInit(){
+  	this.servicio_animales.getAnimales().subscribe(data =>{
+  		console.log(data);
+      this.animales = data.animales;
+      console.log(this.animales);
+  	});
   }
 
   verCal(){
   	this.mostrar_calificaciones = !this.mostrar_calificaciones;
   	console.log("Se presiono el boton");
+  }
+
+  agregar(calificacion){
+  	console.log(calificacion.value);
+  	this.calificaciones.push(calificacion.value);
+  	return false;
   }
 
 
--- src/app/ejemplo.service.ts
+++ src/app/ejemplo.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EjemploService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getAnimales(){
+  	return this.http.get("http://demo0253989.mockable.io/curso-ionic-list");
+  }
+
+  getAlumnos(){
+  	
+  }
 }
